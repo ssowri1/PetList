8018b47ab08e2afc5eb6e09714a30ddcaaaf5398
Sowrirajan Sugumaran <ssowri1@gmail.com>
2023-03-07 20:19:15 +0530

    Initial Commit

---
+++ PetListUITests/PetListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PetListUITestsLaunchTests.swift
+//  PetListUITests
+//
+//  Created by Sowrirajan S on 07/03/23.
+//
+
+import XCTest
+
+final class PetListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
